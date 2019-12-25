87a8b302fce3a12df243c37e99b14c21133e0664
Alexander Bispo <alexbispo173@gmail.com>
2019-12-25 22:56:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
       erb :super_hero
     end
     
-    get '/teams' do
+    get 'contr/teams' do
       @team = Team.new(params[:team])
       
       params[:team][:members].each do |member_params|
